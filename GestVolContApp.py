424c5a9145c88249cd3a3ac3edbcb25bf2b41ec4
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-17 22:34:30 +0300

    Initialized and started the eel App in the py file

--- GestVolContApp.py
+++ GestVolContApp.py
@@ -9,3 +9,12 @@
 
 # Importing the required libraries for the project
 import eel
+
+
+if __name__ == '__main__':
+
+    # Initializing the eel App
+    eel.init("web")
+
+    # Starting the eel App with index.html file and the window size
+    eel.start('index.html', size=(1000, 740))
