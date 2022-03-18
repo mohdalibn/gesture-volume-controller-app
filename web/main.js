0bec565dc9bd04f1cd038918f21de5ffae78f148
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-18 11:11:52 +0300

    Add functionality to hide and show Video Text

--- web/main.js
+++ web/main.js
@@ -8,3 +8,16 @@ value.textContent = slider.value;
 slider.oninput = function(){
     value.textContent = this.value;
 }
+
+
+// Script for Hiding and Showing The Webcam Video Text
+
+const VideoText = document.querySelector(".webcam-off-text")
+
+function hideText(){
+    VideoText.style.display="none";
+}
+
+function showText(){
+    VideoText.style.display="block";
+}
